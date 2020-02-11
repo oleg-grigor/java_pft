4a7e6bdbfe7b710d850634f7e78b66b75748883d
oleg-grigor <ext.oleg.grigorev@tieto.com>
2020-02-11 21:48:41 +0300

    Промежуточный (форматирование)

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Dist.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Dist.java
@@ -1,17 +1,12 @@
 package ru.stqa.pft.sandbox;
 
-import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
-
 public class Dist {
 
     public static void main(String[] args) {
         Point p1 = new Point(2, 5);
         Point p2 = new Point(1, 9);
 
-
         System.out.println("Расстояние между точками = " + p2.distance(p1, p2));
 
     }
-
-
 }
