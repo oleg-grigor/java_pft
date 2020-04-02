8b800b919e80fcced15198aa5146260c5ef9c06e
oleg-grigor <ext.oleg.grigorev@tieto.com>
2020-04-03 01:08:00 +0300

    промежуточный

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/ResetPasswordTests.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/ResetPasswordTests.java
@@ -2,7 +2,14 @@ package ru.stqa.pft.mantis.tests;
 
 import org.testng.annotations.Test;
 
+import java.io.IOException;
+
 public class ResetPasswordTests extends TestBase{
   @Test
-  public void 
+  public void testResetPassword() throws IOException {
+    app.newSession().login("root", "root");
+
+
+
+  }
 }
