3f0ee192cb73724258684ef33c931d2abc3e66f1
oleg-grigor <ext.oleg.grigorev@tieto.com>
2020-04-08 19:48:10 +0300

    Лекция 9.6

---
+++ github-sample/src/test/java/ru/stqa/pft/github/GithubTests.java
@@ -0,0 +1,19 @@
+package ru.stqa.pft.github;
+
+import com.google.common.collect.ImmutableMap;
+import com.jcabi.github.*;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+public class GithubTests {
+
+  @Test
+  public void testCommits() throws IOException {
+    Github github = new RtGithub("2845f772cf9b43accfd411894859871eb3084726");
+    RepoCommits commits = github.repos().get(new Coordinates.Simple("oleg-grigor", "java_pft")).commits();
+    for (RepoCommit commit : commits.iterate(new ImmutableMap.Builder<String, String>().build())) {
+      System.out.println(new RepoCommit.Smart(commit).message());
+    }
+  }
+}
